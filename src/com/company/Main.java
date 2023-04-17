3250aa96717917d08fe76d359c86af299021bea5
KateChubarava <k.chubarava@altoros.com>
2023-04-17 17:51:04 +0200

    test 2 commit 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// 11th commit
+
     }
 }
-//haha3 just for checking
-//haha4 just for checking
+// test 2 commit 1
